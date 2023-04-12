630f3138818eac7b949de42800087fb5744d59df
shen <72540725+shenjke@users.noreply.github.com>
2023-04-12 17:47:50 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,25 +1,31 @@
+from logic import *
 import discord
 
-# Переменная intents - хранит привилегии бота
 intents = discord.Intents.default()
-# Включаем привелегию на чтение сообщений
+
 intents.message_content = True
-# Создаем бота в переменной client и передаем все привелегии
+
 client = discord.Client(intents=intents)
+
 
 @client.event
 async def on_ready():
     print(f'We have logged in as {client.user}')
+
 
 @client.event
 async def on_message(message):
     if message.author == client.user:
         return
     if message.content.startswith('$hello'):
-        await message.channel.send("Hi!")
-    elif message.content.startswith('$bye'):
-        await message.channel.send("\\U0001f642")
+        await message.channel.send('Привет! Я бот!')
+    elif message.content.startswith('$smile'):
+        await message.channel.send(gen_emodji())
+    elif message.content.startswith('$coin'):
+        await message.channel.send(flip_coin())
+    elif message.content.startswith('$pass'):
+        await message.channel.send(gen_pass(10))
     else:
-        await message.channel.send(message.content)
+        await message.channel.send("Я не понимаю такую команду!")
 
-client.run("code")
+client.run("token")
