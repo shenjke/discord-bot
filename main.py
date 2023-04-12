6e8ce43a4514ff8ce67aeea2189c9aa48b9904f7
shen <72540725+shenjke@users.noreply.github.com>
2023-04-12 19:13:48 +0300

    Update main.py

--- main.py
+++ main.py
@@ -7,33 +7,6 @@ intents.message_content = True
 
 client = discord.Client(intents=intents)
 
-class MyClient(discord.Client):
-    async def on_ready(self):
-        print(f'Logged in as {self.user} (ID: {self.user.id})')
-        print('------')
-
-    async def on_message(self, message):
-        if message.content.startswith('!deleteme'):
-            msg = await message.channel.send('I will delete myself now...')
-            await msg.delete()
-
-            # this also works
-            await message.channel.send('Goodbye in 3 seconds...', delete_after=3.0)
-
-    async def on_message_delete(self, message):
-        msg = f'{message.author} has deleted the message: {message.content}'
-        await message.channel.send(msg)
-
-
-intents = discord.Intents.default()
-intents.message_content = True
-
-client = MyClient(intents=intents)
-client.run('token')
-
-@client.event
-async def on_ready():
-    print(f'We have logged in as {client.user}')
 
 
 @client.event
@@ -48,6 +21,9 @@ async def on_message(message):
         await message.channel.send(flip_coin())
     elif message.content.startswith('$pass'):
         await message.channel.send(gen_pass(10))
+    elif message.content.startswith('https://'):
+        await message.delete()
+        await message.channel.send(f"{message.author.mention} Не отправляй ссылки")
     else:
         await message.channel.send("Я не понимаю такую команду!")
 
